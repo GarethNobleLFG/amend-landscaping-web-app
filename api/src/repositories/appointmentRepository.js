e7b6a114a6cce43b3e7add0d469d5fcf82a65132
Gareth Noble <garethnoble3@gmail.com>
2026-05-23 21:13:49 -0400

    ci: pipeline added

--- api/src/repositories/appointmentRepository.js
+++ api/src/repositories/appointmentRepository.js
@@ -1,63 +1,38 @@
 const Appointment = require('../models/appointments'); 
 
 const create = async (appointmentData) => {
-  try {
-    return await Appointment.create(appointmentData);
-  } 
-  catch (error) {
-    throw error;
-  }
+  return await Appointment.create(appointmentData);
 };
 
 const findAll = async () => {
-  try {
-    return await Appointment.findAll({
-      order: [['createdAt', 'DESC']] 
-    });
-  } 
-  catch (error) {
-    throw error;
-  }
+  return await Appointment.findAll({
+    order: [['createdAt', 'DESC']] 
+  });
 };
 
 const findById = async (id) => {
-  try {
-    return await Appointment.findByPk(id);
-  } 
-  catch (error) {
-    throw error;
-  }
+  return await Appointment.findByPk(id);
 };
 
 const update = async (id, updateData) => {
-  try {
-    const appointment = await Appointment.findByPk(id);
-    
-    if (!appointment) {
-      return null; 
-    }
-
-    return await appointment.update(updateData);
-  } 
-  catch (error) {
-    throw error;
+  const appointment = await Appointment.findByPk(id);
+  
+  if (!appointment) {
+    return null; 
   }
+
+  return await appointment.update(updateData);
 };
 
 const deleteAppointment = async (id) => {
-  try {
-    const appointment = await Appointment.findByPk(id);
-    
-    if (!appointment) {
-      return false;
-    }
-
-    await appointment.destroy();
-    return true; 
-  } 
-  catch (error) {
-    throw error;
+  const appointment = await Appointment.findByPk(id);
+  
+  if (!appointment) {
+    return false;
   }
+
+  await appointment.destroy();
+  return true; 
 };
 
 module.exports = {
