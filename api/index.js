15424034ed609c74349049a47f4b689458b1343b
Gareth Noble <garethnoble3@gmail.com>
2026-06-03 21:18:06 -0400

    feat: added more granular buckets

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const httpRequestDurationMicroseconds = new client.Histogram({
   name: 'http_request_duration_seconds',
   help: 'Duration of HTTP requests in seconds',
   labelNames: ['method', 'route', 'code'],
-  buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 10] // ranges in seconds
+  buckets: [0.001, 0.005, 0.01, 0.05, 0.1, 0.3, 0.5, 1, 3, 5, 10]
 });
 
 // Middleware to parse JSON bodies
